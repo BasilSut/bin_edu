53e9ef98bf09e4fcf39836e1436696dfef04d3b4
Basil Sut <1vasilisk12@gmail.com>
2021-09-22 02:37:11 +0300

    plot curved line

--- line.cpp
+++ line.cpp
@@ -2,16 +2,19 @@
 #include "gnuplot.h"
 
 int main(){
-    int x[2][2]=
+    int x[5][2]=
     {
         { 1, 2 } , // строка №0
         { 2, 4 } , // строка №1
-
+        { 3, 8 } , // строка №2...
+        { 4, 16 }, 
+        { 5, 32 }
     };
+
     std::ofstream out_line("./li.dat");//, std::ios::app';
     if (out_line.is_open()){
         
-        for(int i = 0; i<2; ++i){
+        for(int i = 0; i<5; ++i){
             for(int h = 0; h<2; ++h)
                 out_line<<x[i][h]<<"\t";
             out_line<<"\n";
